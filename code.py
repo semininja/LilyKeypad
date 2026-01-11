2af60f01c4c52f21cfba3a6756f13b3eee93262f
semininja <semininja@users.noreply.github.com>
2026-01-11 18:10:34 -0500

    break out layout into separate file

--- code.py
+++ code.py
@@ -3,32 +3,23 @@ import keypad
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode as KC
 from time import monotonic as check
+from layout import layout_matrix
 
 pins = [getattr(board, f'D{i}') for i in range(11)] #list of pins D[i]
 key_matrix = keypad.KeyMatrix(
     row_pins = pins[0:5],
     column_pins = pins[5:11],
-    interval=0.005, #debounce time
+    interval=0.05, #debounce time
     columns_to_anodes=False
 )
-
-layout_matrix = [ #grouped by row, column, layer
-    [['a', '*'], ['b'], ['c'], ['d'], ['is', '\\trill'], ["'"]],
-    [['r', 'R', 's'], ['e'], ['f', '\\relative f'], ['g'], ['es', '\\turn'], [',']],
-    [['1', '1', '6',], ['2', '2', '7', '\\tuplet 3/2'], ['4', '3', '8', '\partial'], ['8', '4', '9'], ['16', '5', '0'], ['.', ' ~', '\\fermata']],
-    [['->', '-^', '--'], ['-.', '-+', '\\open'], ['\\p', '\\pp', '\\mp'],
-        ['\\f', '\\mf', '\\ff'], ['\\!', '\\>', '\\<', '\\n'],
-        [' |\n', '\\bar "||"', '\\bar "|."']],
-    [[' ', '\\', '/'], ['(', '<', '['], ['mod1', 'mod1', KC.LEFT_ARROW, 'mod1'], ['mod2', KC.RIGHT_ARROW, 'mod2', 'mod2'],
-        [')', ']', '>'], [KC.ENTER, KC.BACKSPACE, KC.TAB]]]
 
 hid_kbd = Keyboard(usb_hid.devices)
 hid_layout = KeyboardLayoutUS(hid_kbd)
 event = keypad.Event()
 layer = 0
 layer_set = 0
+num = 0
 
 def tick(delay):
     return check() + delay
@@ -44,6 +35,9 @@ while True:
                 if 'mod' in obj:
                     mark = tick(0.05)
                     layer_set += int(obj[3])
+                elif obj.isdigit():
+                    num += int(obj)
+                    print(num)
                 else:
                     hid_layout.write(obj)
                     print(obj+' sent')
@@ -51,16 +45,20 @@ while True:
                 hid_kbd.press(obj)
                 print(f'{obj} pressed')
         else:
-            try:
-                hid_kbd.release(obj)
-                print(f'{obj} released')
-            except TypeError:
-                if 'mod' in obj:
+                if isinstance(obj, int):
+                    hid_kbd.release(obj)
+                    print(f'{obj} released')
+                elif obj.isdigit():
+                    if num > 0:
+                        hid_layout.write(str(num))
+                        num = 0
+                        print(num)
+                elif 'mod' in obj:
                     layer_set -= min(layer_set, int(obj[3]))
                     hid_kbd.release_all()
                     print('all released')
         
-        print(f'key {event.key_number} {event.pressed} layer {layer}')
+        print(f'key {event.key_number} {event.pressed} layer {layer} obj {obj}')
     elif layer_set != layer:
         if check() > mark:
             layer = layer_set
